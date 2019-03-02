d1a169007b48a01d8fe9f0a1f85b45bcc8a8e0f5
niko <1194142140@qq.com>
2019-03-03 00:36:12 +0800

    tomcat 入口文件

--- src/main/java/net/xiaomotou/freight/ServletInitializer.java
+++ src/main/java/net/xiaomotou/freight/ServletInitializer.java
@@ -3,6 +3,9 @@ package net.xiaomotou.freight;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * tomcat 入口文件一定要加上
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
