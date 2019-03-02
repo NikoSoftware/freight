296a8a5d2f60515501651a804d5d75f620f14e3b
niko <1194142140@qq.com>
2019-03-02 23:58:33 +0800

    优化提交

--- src/main/java/net/xiaomotou/freight/controller/ManagerController.java
+++ src/main/java/net/xiaomotou/freight/controller/ManagerController.java
@@ -3,17 +3,14 @@ package net.xiaomotou.freight.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ManagerController {
 
-
     @RequestMapping("/")
     public String getJson(){
-        ModelAndView modelAndView = new ModelAndView();
 
-        return "{msg:\"这是一个json\"}";
+        return "{msg:\"这是一个json测试\"}";
     }
 
 }
