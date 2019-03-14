24a626cd25c4407d762e96371d513f5e2f137656
niko <1194142140@qq.com>
2019-03-15 00:13:37 +0800

    优化

--- src/main/java/net/xiaomotou/freight/controller/ManagerController.java
+++ src/main/java/net/xiaomotou/freight/controller/ManagerController.java
@@ -255,6 +255,10 @@ public class ManagerController {
         for (int i = 0; i <4 ; i++) {
             ImageModel imageModel = new ImageModel();
             imageModel.setImgPath("/image/pexels-photo-"+(i+1)+".jpeg");
+            if(i==0){
+                imageModel.setUrl("https://www.xiaomotou.net");
+            }
+
             list.add(imageModel);
         }
         priceInfo.setImageList(list);
